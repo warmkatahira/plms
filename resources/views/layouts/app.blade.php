3ece63755067cc213e878201b3b1eb60f0d54a19
warmkatahira <t.katahira@warm.co.jp>
2026-01-07 10:14:36 +0900

    20260107_1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,7 @@
                 <!-- ページヘッダー -->
                 <x-page-header />
                 <!-- プロフィール -->
-                <a class="btn tippy_profile ml-auto" href="{{ route('profile.index') }}" data-full-name="{{ Auth::user()->full_name }}">
+                <a class="btn tippy_profile ml-auto" href="{{ route('profile.index') }}" data-full-name="{{ Auth::user()->user_name }}">
                     <img id="profile" class="profile_image_navigation" src="{{ asset('storage/profile_images/' . Auth::user()->profile_image_file_name) }}">
                 </a>
             </div>
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -9,14 +9,13 @@
         <form method="POST" action="{{ route('profile_update.update') }}" id="profile_update_form">
             @csrf
             <div class="flex flex-col mt-5">
-                <x-form.input type="text" label="姓" id="last_name" name="last_name" :value="$user->last_name" required="true" />
-                <x-form.input type="text" label="名" id="first_name" name="first_name" :value="$user->first_name" required="true" />
-                <x-form.input type="text" label="メールアドレス" id="email" name="email" :value="$user->email" required="true" />
-                <x-form.input type="tel" label="1ページあたりの表示数" id="per_page" name="per_page" :value="$user->per_page" required="true" />
+                <x-form.p label="営業所名" :value="$user->base->base_name" />
+                <x-form.p label="従業員番号" :value="$user->employee_no" />
+                <x-form.p label="氏名" :value="$user->user_name" />
             </div>
         </form>
         <div class="flex flex-row gap-10 mt-5">
-            <button id="profile_update_enter" type="button" class="btn bg-btn-enter text-white w-56 p-3"><i class="las la-check la-lg mr-1"></i>プロフィール更新</button>
+            {{-- <button id="profile_update_enter" type="button" class="btn bg-btn-enter text-white w-56 p-3"><i class="las la-check la-lg mr-1"></i>プロフィール更新</button> --}}
             <button id="logout_enter" type="button" class="btn bg-btn-cancel text-white w-56 p-3"><i class="las la-sign-out-alt la-lg mr-1"></i>ログアウト</button>
         </div>
         <form method="POST" action="{{ route('logout') }}" id="logaut_form" class="mt-5">
