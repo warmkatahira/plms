9b76d75736c047156687ac63bccd5e8b7fc2aee6
warmkatahira <t.katahira@warm.co.jp>
2026-01-19 08:59:35 +0900

    20260119_1

--- resources/views/components/admin/employee/operation-div.blade.php
+++ resources/views/components/admin/employee/operation-div.blade.php
@@ -4,7 +4,7 @@
         <div class="dropdown-content" id="dropdown-content">
             <a href="{{ route('employee_download.download') }}" class="dropdown-content-element"><i class="las la-download la-lg mr-1"></i>ダウンロード</a>
             @can('admin_check')
-                <a href="{{ route('employee_create.index') }}" class="dropdown-content-element"><i class="las la-plus la-lg mr-1"></i>従業員追加(入力)</a>
+                <a href="{{ route('employee_create.index') }}" class="dropdown-content-element"><i class="las la-pen la-lg mr-1"></i>従業員追加(入力)</a>
                 <form method="POST" action="{{ route('employee_create.import') }}" id="employee_create_import_form" enctype="multipart/form-data" class="m-0">
                     @csrf
                     <div class="flex select_file dropdown-content-element">
