7d992135885285a7bb67e75fa7080c5354c0fba1
warmkatahira <t.katahira@warm.co.jp>
2026-01-19 14:55:16 +0900

    20260119_1

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends Controller
         // ページヘッダーをセッションに格納
         session(['page_header' => 'ダッシュボード']);
         // 自身の情報を取得
-        $employee = User::find(Auth::user()->user_no)->with(['base', 'paid_leave', 'statutory_leave']);
+        $employee = User::with(['base', 'paid_leave', 'statutory_leave'])->find(Auth::user()->user_no);
         return view('dashboard')->with([
             'employee' => $employee,
         ]);
