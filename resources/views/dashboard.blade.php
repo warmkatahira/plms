eeea92fc1e8be6f5d208c476dbed88dbe3d27ea7
warmkatahira <t.katahira@warm.co.jp>
2026-01-23 15:24:44 +0900

    20260123_1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout>
     @if(Auth::user()->role_id === RoleEnum::USER)
         <div class="grid grid-cols-12 gap-3">
+            <div class="col-span-12 md:hidden bg-white rounded-xl shadow-lg p-4">
+                <p class="text-2xl font-bold text-gray-800 text-center">{{ Auth::user()->user_name }}<span class="text-base font-normal text-gray-400 pl-3">さん</span></p>
+            </div>
             <div class="col-span-12 grid grid-cols-12 gap-3">
                 <x-dashboard.info-div label="有給保有日数" :value="$employee->paid_leave->paid_leave_granted_days" format="day" />
                 <x-dashboard.info-div label="有給残日数" :value="$employee->paid_leave->paid_leave_remaining_days" format="day" />
