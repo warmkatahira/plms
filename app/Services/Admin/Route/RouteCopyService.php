a135c3b4a40cc2219a42bac9046a7162c24e90f4
warmkatahira <t.katahira@warm.co.jp>
2026-02-05 15:26:47 +0900

    20260205_1

--- app/Services/Admin/Route/RouteCopyService.php
+++ app/Services/Admin/Route/RouteCopyService.php
@@ -13,9 +13,15 @@ class RouteCopyService
     {
         // 複製元となるルートを取得
         $route = Route::byPk($request->route_id)->with('route_details')->lockForUpdate()->firstOrFail();
+        // ルート名のベースとなる名前を変数に格納
+        $base_route_name = '(複製)' . $route->route_name;
+        // ルート名を条件にカウント
+        $count = Route::where('route_name', 'like', $base_route_name . '%')->count();
+        // 今回つけるルート名を変数に格納
+        $new_route_name = $count === 0 ? $base_route_name : $base_route_name . '(' . ($count + 1) . ')';
         // 親(routes)を複製
         $newRoute = $route->replicate();
-        $newRoute->route_name = '(複製)'.$route->route_name;
+        $newRoute->route_name = $new_route_name;
         $newRoute->created_at = now();
         $newRoute->updated_at = now();
         $newRoute->save();
