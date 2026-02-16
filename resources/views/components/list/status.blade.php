1eb59472c9a00784e1a03a801239c93b44d128f6
warmkatahira <t.katahira@warm.co.jp>
2026-02-16 10:10:30 +0900

    20260216_1

--- resources/views/components/list/status.blade.php
+++ resources/views/components/list/status.blade.php
@@ -4,6 +4,7 @@
     'label0',
 ])
 
+
 @if($value)
     <span class="inline-flex items-center gap-1 px-2 py-0.5 text-xs font-semibold text-status-active-text bg-status-active-bg rounded-full">
         <span class="w-2 h-2 bg-status-active-dot rounded-full"></span>{{ $label1 }}
