8e110635412df922108226ba915e2bdaeb7c3c88
warmkatahira <t.katahira@warm.co.jp>
2026-03-09 14:25:05 +0900

    20260309_1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,9 +5,9 @@
                 <p class="text-2xl font-bold text-gray-800 text-center">{{ Auth::user()->user_name }}<span class="text-base font-normal text-gray-400 pl-3">さん</span></p>
             </div>
             <div class="col-span-12 grid grid-cols-12 gap-3">
-                <x-dashboard.info-div label="有給保有日数" :value="$employee->paid_leave->paid_leave_granted_days" format="day" />
-                <x-dashboard.info-div label="有給取得日数" :value="$employee->paid_leave->paid_leave_used_days" format="day" usedDays="true"  />
-                <x-dashboard.info-div label="有給残日数" :value="$employee->paid_leave->paid_leave_remaining_days" format="day" />
+                <x-dashboard.info-div label="保有日数" :value="$employee->paid_leave->paid_leave_granted_days" format="day" />
+                <x-dashboard.info-div label="取得日数" :value="$employee->paid_leave->paid_leave_used_days" format="day" usedDays="true"  />
+                <x-dashboard.info-div label="残日数" :value="$employee->paid_leave->paid_leave_remaining_days" format="day" />
             </diiv>
             <div class="col-span-12 grid grid-cols-12 gap-3">
                 <x-dashboard.info-div label="義務の日数" :value="$employee->statutory_leave->statutory_leave_days" format="day" />
