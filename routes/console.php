4bc4044d1930dee41bf48ae4a21ca4c24804204a
warmkatahira <t.katahira@warm.co.jp>
2026-06-19 13:37:50 +0900

    20260619__1

--- routes/console.php
+++ routes/console.php
@@ -9,7 +9,5 @@ use Illuminate\Support\Facades\Schedule;
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-   Daily   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 // DBバックアップの削除を毎日「03:00」に実行
 Schedule::command('backup_db_delete')->dailyAt('03:00');
-// DBバックアップを毎日「03:30」に実行
-Schedule::command('backup:run --disable-notifications --only-db --only-to-disk=db_backup_normal')->dailyAt('03:30');
 // DBバックアップを毎月20日「03:30」に実行
 Schedule::command('backup:run --disable-notifications --only-db --only-to-disk=db_backup_normal')->monthlyOn(20, '03:30');
